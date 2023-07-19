dc961420ec7c8e9f6fde4898938f228361ace6de
boubina-UNav <139898300+boubina-UNav@users.noreply.github.com>
2023-07-19 10:11:53 +0200

    Add files via upload

---
+++ clean.R
@@ -0,0 +1,44 @@
+taboa <- read.table("united.csv",sep=',')
+indSi <- which(grepl(".bmp",taboa[,1]))
+indNom <- which(grepl(".csv",taboa[,1])) 
+
+nSi <- length(indSi)
+Adh <- rep("adhesive",nSi)
+Tadh <- 1:nSi
+Tair <- Tadh
+Qadh <- Tadh
+
+anotando <- 1
+indNom <- c(indNom,dim(taboa)[1]+1)
+for (i in 1:(length(indNom)-1)) {
+  nome <- as.character(taboa[indNom[i],1])
+  numero <- indNom[i+1] - indNom[i] - 2
+  indices <- anotando:(anotando+numero-1)
+  anotando <- anotando + numero
+  if (grepl("PHL4150",nome)) {
+    Adh[indices] <- rep("Clarity PHL 4150",numero)
+    Tadh[indices] <- rep(145,numero)
+    if (grepl("Warm",nome)) Tair[indices] <- rep(170,numero)
+    else Tair[indices] <- rep(110,numero)
+    if (grepl("/Q1/",nome)) Qadh[indices] <- rep(1,numero)
+    else if (grepl("/Q2/",nome)) Qadh[indices] <- rep(2.8,numero)
+    else Qadh[indices] <- rep(3.9,numero)
+  }
+  else {
+    Tadh[indices] <- rep(155,numero)
+    if (grepl("Warm",nome)) Tair[indices] <- rep(180,numero)
+    else Tair[indices] <- rep(120,numero)
+    if (grepl("/Q1/",nome)) Qadh[indices] <- rep(1,numero)
+    else if (grepl("/Q2/",nome)) Qadh[indices] <- rep(2,numero)
+    else Qadh[indices] <- rep(3,numero)
+    if (grepl("PHC7194",nome)) Adh[indices] <- rep("Lunatack PHC 7194",numero)
+    else Adh[indices] <- rep("Lunatack D 7214",numero)
+  }
+}
+
+taboa <- cbind(taboa[indSi,-1],Adh,Tadh,Qadh,Tair)
+colnames(taboa) <- c('P(mbar)', "h(mm)", "f(Hz)", "std(f)(Hz)", "d(mm)", "std(d)(mm)", "Adhesive", "Tadh(C)", "Qadh(g/s)", "Tair (C)")
+limpa <- taboa[as.character(taboa$`std(f)(Hz)`)!="NaN",]
+row.names(limpa) <- NULL
+#for (i in 1:6) limpa[,i] <- as.numeric(as.character(limpa[,i]))
+write.table(limpa,"clean.csv",sep=',',row.names=FALSE)
